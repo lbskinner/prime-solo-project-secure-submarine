7149a09b1f8725ab52562ebf4840f4af214c119e
Leila Skinner <beileiwang@gmail.com>
2020-04-30 13:20:35 -0500

    run build to create a build folder

---
+++ build/precache-manifest.a74b8cab1ca19b523a2f46a7d79785ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de5744a9d1adda9f6de45854642ef25f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6db0b9f98509ac08b115",
+    "url": "/static/css/main.c079c376.chunk.css"
+  },
+  {
+    "revision": "581278ae0049aadc4954",
+    "url": "/static/js/2.38cb3af7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.38cb3af7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6db0b9f98509ac08b115",
+    "url": "/static/js/main.bfad464b.chunk.js"
+  },
+  {
+    "revision": "8661bc4c26bf8170c271",
+    "url": "/static/js/runtime-main.6e4f4f5c.js"
+  },
+  {
+    "revision": "7ce9680e738565539c06cf0326d7302f",
+    "url": "/static/media/submarine.7ce9680e.svg"
+  }
+]);
